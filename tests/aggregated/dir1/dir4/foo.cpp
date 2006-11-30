112ada461ae2e5d8dad1ae4bd8751db77bd17890
elliott_c <ocielliottc@users.noreply.github.com>
2006-11-30 16:07:50 +0000

    ChangeLogTag: Thu Nov 30 15:59:52 UTC 2006  Chad Elliott  <elliott_c@ociweb.com>

---
+++ tests/aggregated/dir1/dir4/foo.cpp
@@ -0,0 +1,9 @@
+#if defined(REQUIRED) || defined(ALSO_REQUIRED)
+The REQUIRED and ALSO_REQUIRED macros should not be set
+#endif
+#if !defined(NEEDED_BY_ALL)
+The NEEDED_BY_ALL macro should be set
+#endif
+#if defined(NEEDED_BY_NONE)
+The NEEDED_BY_NONE macro should not be set
+#endif
