04e9f84d44e8fa5854de5e6a1e408334d5ecedc8
elliott_c <elliott_c@38936888-4219-0410-9517-dfce991c981b>
2006-12-15 13:09:16 +0000

    ChangeLogTag: Fri Dec 15 13:08:36 UTC 2006  Chad Elliott  <elliott_c@ociweb.com>

--- trunk/utils/add_expected_directories.sh
+++ trunk/utils/add_expected_directories.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
 #
 # 1) Change to the expected directory
-# 2) svn add the top level test directory
-# 3) Run this script giving it the full path to execute it
+# 2) Run this script with the full path to execute it
 
 for i in *; do
   if [ -d $i ]; then
     if [ "$i" != ".svn" ]; then
-      echo "Adding directory: $i"
-      svn add $i
-      (cd $i; $0)
+      if [ ! -e "$i/.svn" ]; then
+        echo "Adding directory: $i"
+        svn add $i
+        (cd $i; $0 propset)
+      else
+        if [ "$1" = "propset" ]; then
+          (cd $i; $0 propset)
+        fi
+      fi
     fi
   else
-    echo "Adding file: $i"
+    echo "Setting properties for file: $i"
     svn propset svn:mime-type application/octet-stream $i
     svn propset svn:keywords "Author Date Id Revision" $i
   fi
