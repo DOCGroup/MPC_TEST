6de5330567eab125c6b452934508774a073c4da1
elliott_c <ocielliottc@users.noreply.github.com>
2007-01-19 16:14:39 +0000

    ChangeLogTag: Fri Jan 19 16:10:50 UTC 2007  Chad Elliott  <elliott_c@ociweb.com>

--- utils/nightly_em3.bat
+++ utils/nightly_em3.bat
@@ -1,10 +1,22 @@
 @echo off
 
-call "C:\Program Files\Microsoft eMbedded C++ 4.0\EVC\wce420\bin\WCEemulator.BAT"
+set BASE_DIR=Z:
+set WCEROOT=Z:\Microsoft eMbedded C++ 4.0
+set SDKROOT=Z:\Windows CE Tools
 
-set ACE_ROOT=
-set MPC_COMMANDLINE=
 set MPC_ROOT=u:\current\MPC
 cd /d %MPC_ROOT%\test
 
+if exist "%WCEROOT%" goto start
+echo "%BASE_DIR%\ is not mapped" > pick_me_up_em3
+goto done
+
+:start
+call "%WCEROOT%\EVC\wce420\bin\WCEemulator.BAT"
+
+set ACE_ROOT=
+set MPC_COMMANDLINE=
+
 run_tests.pl --type em3 --output pick_me_up_em3
+
+:done
--- utils/nightly_vc6.bat
+++ utils/nightly_vc6.bat
@@ -1,12 +1,28 @@
 @echo off
+set BASE_DIR=Z:
+set MSBaseDir=Z:\Microsoft Visual Studio
 
-call "C:\Program Files\Microsoft Visual Studio\VC98\Bin\VCVARS32.BAT"
+set MPC_ROOT=u:\current\MPC
+cd /d %MPC_ROOT%\test
+
+if exist "%MSBaseDir%" goto start
+echo "%BASE_DIR%\ is not mapped" > pick_me_up_vc6
+goto done
+
+:start
+rem call "C:\Program Files\Microsoft Visual Studio\VC98\Bin\VCVARS32.BAT"
+set MSDevDir=%MSBaseDir%\Common\MSDev98
+set MSVCDir=%MSBaseDir%\VC98
+set PATH=%MSDevDir%\BIN;%MSVCDir%\BIN;%PATH%
+set INCLUDE=%MSVCDir%\ATL\INCLUDE;%MSVCDir%\INCLUDE;%MSVCDir%\MFC\INCLUDE;%INCLUDE%
+set LIB=%MSVCDir%\LIB;%MSVCDir%\MFC\LIB;%LIB%
+
 set PERL5_INCLUDE=C:\Perl\lib\CORE
 set PERL5_LIB=perl58
 
 set ACE_ROOT=
 set MPC_COMMANDLINE=
-set MPC_ROOT=u:\current\MPC
-cd /d %MPC_ROOT%\test
 
-run_tests.pl --type nmake --type vc6 --output pick_me_up_vc6
+run_tests.pl --type vc6 --output pick_me_up_vc6
+
+:done
