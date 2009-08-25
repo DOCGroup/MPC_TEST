37f84880c66fe5fb04c7f58e97a1f3537bb4526f
elliott_c <ocielliottc@users.noreply.github.com>
2009-08-25 17:52:14 +0000

    ChangeLogTag: Tue Aug 25 17:50:48 UTC 2009  Chad Elliott  <elliott_c@ociweb.com>

--- utils/add_expected_files.sh
+++ utils/add_expected_files.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for i in *; do
+for i in $*; do
   echo "Setting properties for file: $i"
   svn propset svn:mime-type application/octet-stream $i
   svn propset svn:keywords "Author Date Id Revision" $i
