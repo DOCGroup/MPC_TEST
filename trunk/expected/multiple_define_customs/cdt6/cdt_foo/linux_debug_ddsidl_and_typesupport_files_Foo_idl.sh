7a623430a65cffe16328f39b52140c227fc15fdf
mitza <mitza@38936888-4219-0410-9517-dfce991c981b>
2010-11-01 19:11:35 +0000

    ChangeLogTag: Mon Nov  1 19:14:54 UTC 2010  Adam Mitz  <mitza@ociweb.com>

---
+++ trunk/expected/multiple_define_customs/cdt6/cdt_foo/linux_debug_ddsidl_and_typesupport_files_Foo_idl.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd ../..
+perl zzz.pl  "Foo.idl" && perl foo.pl  "Foo.idl" && cd cdt_foo/linux-debug && ln -sf ../../FooD.cpp . && ln -sf ../../FooD.h . && ln -sf ../../FooD.idl . && ln -sf ../../FooTypeSupportImpl.cpp . && ln -sf ../../FooTypeSupportImpl.h .
